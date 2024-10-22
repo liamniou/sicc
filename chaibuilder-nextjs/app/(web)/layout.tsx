44360a6c4c8428dc9ac4206c8aaee226549add6f
Stanislau Liamniou <stanislau.liamniou@epidemicsound.com>
2024-10-22 15:49:46 +0200

    Init

---
+++ chaibuilder-nextjs/app/(web)/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "@/app/globals.css";
+import PrelineScript from "@/app/(web)/PrelineScripts";
+
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "SICC",
+  description: "Site of Stockholm International Cinema Collective",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="light">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 dark:bg-gray-900`}
+      >
+        {children}
+        <PrelineScript />
+      </body>
+    </html>
+  );
+}
